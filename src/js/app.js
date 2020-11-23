6ef882fb718c4f95e375a369cdb75120586adda5
Ilya Tatarinov <itatarinov7@gmail.com>
2020-11-23 19:43:17 +0300

    HW done

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,20 @@
 console.log('it works!');
 
+import goblin from '../img/goblin.png';
+
+export default function start() {
+  const img = document.createElement('img');
+  img.src = goblin;
+  const id = Math.floor(Math.random() * 16);
+  const td = document.getElementById(`${id}`);
+  td.appendChild(img);
+}
+
+export function next() {
+  const lastImg = document.getElementsByTagName('img')[0];
+  lastImg.remove();
+  start();
+}
+
+start();
+setInterval(next, 1000);
